dce9615d29577aa30071954a31531f4c4c05a987
Clayton <claytonprogrammer@gmail.com>
2026-08-17 11:34:59 -0300

    commit inicial

---
+++ src/main/java/com/javanauta/revisaousuario/RevisaoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.revisaousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisaoUsuarioApplication.class, args);
+	}
+
+}
