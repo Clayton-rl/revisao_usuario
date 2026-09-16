b04a35200c1239cef7a8deac4880b334099193d8
Clayton <claytonprogrammer@gmail.com>
2026-09-16 09:07:16 -0300

    "Refatoração do método de login e autenticação de usuário"

--- src/main/java/com/javanauta/revisaousuario/controller/GlobalExceptionHandler.java
+++ src/main/java/com/javanauta/revisaousuario/controller/GlobalExceptionHandler.java
@@ -4,23 +4,31 @@ import com.javanauta.revisaousuario.infrastructure.exceptions.ConflictException;
 import com.javanauta.revisaousuario.infrastructure.exceptions.IllegalArgumentException;
 import com.javanauta.revisaousuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.revisaousuario.infrastructure.exceptions.UnauthorizedException;
+import com.javanauta.revisaousuario.infrastructure.exceptions.dto.ErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handlerResourceNotFoundException(ResourceNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponseDTO> handlerResourceNotFoundException(ResourceNotFoundException ex,
+                                                                             HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(buildError(HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(), "Not Found", request.getRequestURI()));
     }
 
     @ExceptionHandler(ConflictException.class)
-    public ResponseEntity<String> handlerConflictException(ConflictException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    public ResponseEntity<ErrorResponseDTO> handlerConflictException(ConflictException ex,
+                                                                     HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(buildError(HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(), "Not Found", request.getRequestURI()));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
@@ -31,5 +39,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handlerIllegalArgumentException(IllegalArgumentException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    private ErrorResponseDTO buildError(int status, String mensagem, String error, String path) {
+        return ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .message(mensagem)
+                .status(status)
+                .error(error)
+                .path(path)
+                .build();
     }
 }
